705fa4a784cdcb47deb58272e5e9d1513433a3b7
Junior1511 <yunior122000@gmail.com>
2024-02-25 20:24:49 -0600

    starting

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const computerOptions = ["Rock", "Paper" , "Scissors"]
+
+
+function getComputerChoice(array){
+    const randomIndex = Math.floor(Math.random() * array.length);
+
+    return array[randomIndex];
+}
+
+getComputerChoice(computerOptions)
+
+function RPS (playerSelection,computerSelection){
+    
+}
